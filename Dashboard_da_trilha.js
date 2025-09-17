045873b2f022a1e3e46326f3560b8ac72b234392
JORDAN SOARES AGUIAR <jordan_aguiar@estudante.sesisenai.org.br>
2025-09-17 11:19:39 -0300

    atualização

--- Dashboard_da_trilha.js
+++ Dashboard_da_trilha.js
@@ -5,3 +5,31 @@ const carousel = new bootstrap.Carousel(carouselElement, {
   pause: 'hover', // pausa no hover
   wrap: true, // volta ao primeiro slide depois do último
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const imagensComentario = document.querySelector('.imagens_comentario');
+  const imgs = imagensComentario.querySelectorAll('img');
+  const verImagensDiv = imagensComentario.querySelector('.ver_imagens');
+
+  function verificarResolucao() {
+      if (window.innerWidth < 1500) {
+          // Se a resolução for menor que 1500px
+          imgs.forEach(img => {
+              img.style.display = 'none'; // Esconde as imagens
+          });
+          verImagensDiv.style.display = 'flex'; // Mostra a div "ver_imagens"
+      } else {
+          // Se a resolução for 1500px ou maior
+          imgs.forEach(img => {
+              img.style.display = 'block'; // Mostra as imagens
+          });
+          verImagensDiv.style.display = 'none'; // Esconde a div "ver_imagens"
+      }
+  }
+
+  // Executa a função na carga da página
+  verificarResolucao();
+
+  // Adiciona um listener para quando a janela for redimensionada
+  window.addEventListener('resize', verificarResolucao);
+});
