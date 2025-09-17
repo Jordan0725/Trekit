e8ed23b985c5e5bd10efeb3a868b4023a0d0bae6
Jordan0725 <161843035+Jordan0725@users.noreply.github.com>
2025-09-16 22:56:34 -0300

    Add files via upload

--- Dashboard_da_trilha.js
+++ Dashboard_da_trilha.js
@@ -1,6 +1,6 @@
 const carouselElement = document.querySelector('#carouselImagens');
 const carousel = new bootstrap.Carousel(carouselElement, {
-  interval: 3000, // muda slide a cada 3 segundos
+  // interval: 3000, // muda slide a cada 3 segundos
   ride: 'carousel', // inicia automaticamente
   pause: 'hover', // pausa no hover
   wrap: true, // volta ao primeiro slide depois do último
